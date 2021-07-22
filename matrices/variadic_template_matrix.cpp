6036d9f1818499d3c1a93c5b3e99125a898fdbc0
Ali <aliabbas.299792@gmail.com>
2021-07-22 15:04:30 +0100

    Update variadic_template_matrix.cpp
    Now the matrix class can make an n dimensional matrix with configurable dimensions
    i.e
    `matrix<int> mat{3, 4, 5, 2}; // will make a 3x4x5x2 matrix`

--- matrices/variadic_template_matrix.cpp
+++ matrices/variadic_template_matrix.cpp
@@ -1,54 +1,108 @@
 #include <iostream>
+#include <vector>
 
 void fatal_exit(std::string msg, int exit_code = -1){
-  std::cerr << msg << "\n";
+  std::cerr << msg << std::endl;
   std::exit(exit_code);
 }
 
-uint64_t int_pow(uint64_t n, uint64_t exp){
-  if(exp == 0) return 1;
-  if(n < 0 || exp < 1) fatal_exit("Incorrect powers");
-  uint64_t num = 1;
-  while(exp--)
-    num *= n;
-  return num;
-}
-
-template<typename T, int dim_sizes, int dims>
+template<typename T>
 class matrix{
   T *arr = nullptr;
-public:
-  matrix(){
-    arr = new T[int_pow(dim_sizes, dims)];
-  }
+  
+  std::vector<int> dim_sizes{};
+  std::vector<int> dim_offsets{};
+  int dims = -1;
 
   template<typename U, typename... Args>
-  T &operator()(U idx, Args... args){
-    if(idx >= dim_sizes) fatal_exit("Out of bounds access");
+  void construct_matrix(U dim_size, Args... args){
+    dim_sizes.push_back(dim_size);
 
-    int dim_num = dims - 1;
+    construct_matrix(args...);
+  }
 
-    int offset = int_pow(dim_sizes, dim_num) * idx;
+  template<typename U>
+  void construct_matrix(U dim_size){
+    dim_sizes.push_back(dim_size);
+    dims = dim_sizes.size();
 
-    return get_item(offset, dim_num - 1, args...);
+    // need to reverse the sizes, since they are accessed when indexing into the array
+    std::vector<int> reversed_sizes{};
+
+    dim_offsets.resize(dims);
+    dim_offsets[0] = 1;
+
+    int cumulative_size = 1;
+    for(int i = dims - 1; i >= 0; i--){
+      cumulative_size *= dim_sizes[i];
+      reversed_sizes.push_back(dim_sizes[i]);
+
+      dim_offsets[dims-i] = cumulative_size;
+    }
+    
+    dim_sizes = reversed_sizes;
+    
+    // cumulative_size is the total size at this point
+    if(cumulative_size < 1) fatal_exit("Incorrect matrix size");
+
+    arr = new T[cumulative_size];
   }
 
   template<typename U, typename... Args>
   T &get_item(int offset, int dim_num, U idx, Args... args){
-    if(idx >= dim_sizes) fatal_exit("Out of bounds access");
+    if(idx >= dim_sizes[dim_num]) fatal_exit("Out of bounds access");
 
-    offset += int_pow(dim_sizes, dim_num) * idx;
+    offset += dim_offsets[dim_num] * idx;
 
     return get_item(offset, dim_num - 1, args...);
   }
 
   template<typename U>
   T &get_item(int offset, int dim_num, U idx){
-    if(idx >= dim_sizes) fatal_exit("Out of bounds access");
+    if(idx >= dim_sizes[dim_num]) fatal_exit("Out of bounds access");
 
-    offset += int_pow(dim_sizes, dim_num) * idx;
+    offset += dim_offsets[dim_num] * idx;
+
+    if(dim_num != 0) fatal_exit("Incorrect number of indexes");
 
     return arr[offset];
+  }
+public:
+  template<typename U, typename... Args>
+  matrix(U dim_size, Args... args){
+    dim_sizes.push_back(dim_size);
+
+    construct_matrix(args...);
+  }
+
+  matrix(int dim_size){
+    dim_sizes.push_back(dim_size);
+    dims = 1;
+    
+    if(dim_size < 1) fatal_exit("Incorrect matrix size");
+
+    arr = new T[dim_size];
+  }
+
+  template<typename U, typename... Args>
+  T &operator()(U idx, Args... args){
+    int dim_num = dims - 1;
+
+    if(idx >= dim_sizes[dim_num]) fatal_exit("Out of bounds access");
+
+    int offset = dim_offsets[dim_num] * idx;
+
+    return get_item(offset, dim_num - 1, args...);
+  }
+
+  T &operator()(int idx){
+    if(idx >= dim_sizes[0]) fatal_exit("Out of bounds access");
+
+    int dim_num = dims - 1;
+    
+    if(dim_num != 0) fatal_exit("Incorrect number of indexes");
+
+    return arr[idx];
   }
   
   ~matrix(){
@@ -58,32 +112,40 @@ public:
 
 int main()
 {
-  matrix<int, 4, 5> mat{};
+  matrix<int> mat{1, 2, 3, 4, 5, 6, 7};
 
   int count = 0;
-  for(int i = 0; i < 4; i++){
-    for(int j = 0; j < 4; j++){
-      for(int k = 0; k < 4; k++){
+  for(int i = 0; i < 1; i++){
+    for(int j = 0; j < 2; j++){
+      for(int k = 0; k < 3; k++){
         for(int l = 0; l < 4; l++){
-          for(int m = 0; m < 4; m++){
-            mat(i, j, k, l, m) = count++;
+          for(int m = 0; m < 5; m++){
+            for(int n = 0; n < 6; n++){
+              for(int o = 0; o < 7; o++){
+                mat(i, j, k, l, m, n, o) = count++;
+              }
+            }
           }
         }
       }
     }
   }
 
-  for(int i = 0; i < 4; i++){
-    for(int j = 0; j < 4; j++){
-      for(int k = 0; k < 4; k++){
+  for(int i = 0; i < 1; i++){
+    for(int j = 0; j < 2; j++){
+      for(int k = 0; k < 3; k++){
         for(int l = 0; l < 4; l++){
-          for(int m = 0; m < 4; m++){
-            std::cout << mat(i, j, k, l, m) << "\n";
+          for(int m = 0; m < 5; m++){
+            for(int n = 0; n < 6; n++){
+              for(int o = 0; o < 7; o++){
+                std::cout << mat(i, j, k, l, m, n, o) << "\n";
+              }
+            }
           }
         }
       }
     }
   }
   
-  // ends up printing from 0 to 1023
+  // ends up printing from 0 to 5039
 }
